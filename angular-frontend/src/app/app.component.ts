bfc943afc29eab06e0a7d76be94920ea0fa467bf
Alexia <lexi.baco@gmail.com>
2026-03-04 19:13:54 +0100

    Finalizado Ejercicio 3.1: Estructura base y AppComponent

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -8,5 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-frontend';
+  title = 'Mi Angular';
+  subtitle = 'Subtitulo de ejemplo'
 }
